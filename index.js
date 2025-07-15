b1f9023dff54794fe67b160abf2cf7e04b2168a7
Maymoiz <Moishamayndlovu@gmail.com>
2025-07-15 12:38:34 +0200

    added basic javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+function generateRecipe(event) {
+  event.peventDefault();
+
+  new Typewriter("#typewriter", {
+    strings: [
+      "Welcome to Mo's Recipe Generator!",
+      "Let's create a delicious recipe together.",
+      "What ingredients do you have?",
+      "Cooking is an art, let's paint a masterpiece!",
+      "Your culinary adventure starts here!",
+    ],
+    autoStart: true,
+    delay: 75,
+    loop: true,
+  });
+}
+let formElement = document.querySelector("#recipe-form");
+formElement.addEventListener("submit", generateRecipe);
